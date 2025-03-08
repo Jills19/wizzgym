16f2a635f699da2798df271672108df34c5cc8fd
Jills19 <Jills19@gmail.com>
2025-03-08 19:51:03 +0000

    updated design colour

---
+++ _next/static/chunks/app/aboutus/page-b76296172a0b8c97.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[420],{5263:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.bind(n,3074)),Promise.resolve().then(n.bind(n,2356))}},e=>{var s=s=>e(e.s=s);e.O(0,[505,647,356,74,441,517,358],()=>s(5263)),_N_E=e.O()}]);
